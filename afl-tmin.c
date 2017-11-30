d4d663fa94f6ed953084c317833375465ff04ce0
Caroline Lemieux <clemieux@cs.berkeley.edu>
2017-11-29 16:34:51 -0800

    extra alloc size so no segfaults

--- afl-tmin.c
+++ afl-tmin.c
@@ -170,7 +170,7 @@ static void setup_shm(void) {
 
   u8* shm_str;
 
-  shm_id = shmget(IPC_PRIVATE, MAP_SIZE, IPC_CREAT | IPC_EXCL | 0600);
+  shm_id = shmget(IPC_PRIVATE, MAP_SIZE + PERF_SIZE*sizeof(u32), IPC_CREAT | IPC_EXCL | 0600);
 
   if (shm_id < 0) PFATAL("shmget() failed");
 
