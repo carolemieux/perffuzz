a96ed3fb924078113b3797685c0c0fbbdf71f162
Caroline Lemieux <clemieux@cs.berkeley.edu>
2018-01-22 12:32:53 -0800

    have things over the queue/crashes/hangs

--- gen_plot.py
+++ gen_plot.py
@@ -1,9 +1,10 @@
+from __future__ import print_function
 import sys
-#import matplotlib
-#matplotlib.use('Agg')
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import numpy as np
-import matplotlib
+#import matplotlib
 import re
 
 
@@ -28,34 +29,51 @@ def parse_num_inputs_afl(f):
 		if not line.startswith("#"):
 			split_line = line.split(",")
 			raw_times.append(int(split_line[0]))
-			raw_num_inputs.append(int(split_line[3]))
+			raw_num_inputs.append((int(split_line[3]), int(split_line[7]), int(split_line[8])))
 	start_time = raw_times[0]
 	relative_times = [t - start_time for t in raw_times]
 	return relative_times, raw_num_inputs
+
+def progressive_max(arr):
+	return [max(arr[:i+1]) for i in range(len(arr))]
 
 # assumes a file with the structure 
 # data   afl_filename
 # returns number of inputs generated -> max count
 def parse_max_counts_afl(f):
-	fname_re = re.compile("id:(.*),src.*")
-	ret_arr = []
+	fname_re = re.compile(".*(queue|crashes|hangs)/id:(.*),(src|orig).*")
+	queued = []
+	crashes = []
+	hangs = []
 	for line in f:
 		line = line.rstrip()
 		split_line = line.split("\t")
 		match = fname_re.match(split_line[1])
-		if match == None
-			print("Error in parse_max_counts_afl: no match for filename")
-			return
-		id_num = int(match.group(1))
+		if match == None:
+			print("Error in parse_max_counts_afl: no match for filename:\n%s\n" % split_line[1])
+			continue
+		folder = match.group(1)
+		if folder == "queue":
+			ret_arr = queued
+		elif folder == "crashes":
+			ret_arr = crashes
+		elif folder == "hangs":
+			ret_arr = hangs
+		id_num = int(match.group(2))
 		data = int(split_line[0])
 		if id_num < len(ret_arr):
-			assert(data == ret_arr[id_num])
+			if (data != ret_arr[id_num]):
+				print(line)
+				print(id_num)
+				print(ret_arr[id_num])
+			ret_arr[id_num] = max(ret_arr[id_num], data)
+			#assert(data == ret_arr[id_num])
 		elif id_num == len(ret_arr):
 			ret_arr.append(data)
 		else:
 			print("Error: out-of-order printing")
 			return
-	return ret_arr
+	return progressive_max(queued), progressive_max(crashes), progressive_max(hangs)
 
 #assumes a file with structure
 # plot	timestamp
@@ -67,29 +85,39 @@ def parse_max_counts_slow(f):
 		split_line = line.split('\t')
 		raw_max.append(int(split_line[0]))
 		raw_times.append(int(split_line[1]))
-	start_time = raw_times[0]
+	start_time = min(raw_times)
 	relative_times = [t - start_time for t in raw_times]
 	return relative_times, raw_max
+
+def max_at_m(arr1,arr2,arr3,t):
+	m1,m2,m3 = t
+	res1 = 0 if m1 == 0 else arr1[m1-1]
+	res2 = 0 if m2 == 0 else arr2[m2-1]
+	res3 = 0 if m3 == 0 else arr3[m3-1]
+	return max(res1,res2,res3)	
 
 def parse_afl(foldername):
 	plot_data = open(foldername + "/plot_data")
 	times, raw_num_inputs = parse_num_inputs_afl(plot_data)
-	close(plot_data)
 	counts_data = open(foldername + "/counts-and-names")
-	max_counts_per_gen = [counts_data[m-1] for m in raw_num_inputs]
+	queued_counts, crashes_counts, hangs_counts = parse_max_counts_afl(counts_data)
+	max_counts_per_gen = [max_at_m(queued_counts,crashes_counts,hangs_counts,m) for m in raw_num_inputs]
+	return times, max_counts_per_gen
 
 def parse_slow(foldername):
-	file = open(foldername + "/counts-and-times")
+	f = open(foldername + "/counts-and-times")
+	times, raw = parse_max_counts_slow(f)
+	return times, progressive_max(raw)
 
-
-def populate_counts(folder_prefixes, folder_suffixes, all_times):
+def populate_counts(folder_prefixes, folder_suffixes):
 	times = {}
 	counts = {}
 	n = len(folder_suffixes)
 	for prefix in folder_prefixes:
-		times[prefix][i] = [None] * n
-		counts[prefix][i] = [None] * n
+		times[prefix] = [None] * n
+		counts[prefix] = [None] * n
 		for i in range(n):
+			print("parsing %s-%d" % (prefix, i))
 			suffix = folder_suffixes[i]
 			if 'afl' in prefix:
 				times[prefix][i], counts[prefix][i] = parse_afl(prefix + suffix)
@@ -98,7 +126,6 @@ def populate_counts(folder_prefixes, folder_suffixes, all_times):
 			else:
 				print("Not AFL or SLOW... I'm not sure what to do")
 				return
-			all_times = all_times.union(set(times[prefix][i]))
 	return times, counts
 
 
@@ -107,7 +134,8 @@ def populate_counts(folder_prefixes, folder_suffixes, all_times):
 # def main():
 if len (sys.argv) < 3:
 	print("usage: python [-i] %s [numreps] [prefixes] [names]" % sys.argv[0])
-num_prefixes = (len(sys.argv) -2 )/2
+	exit(1)
+num_prefixes = int((len(sys.argv) -2 )/2)
 for i in range(2, num_prefixes + 2):
 	folder_prefixes.append(sys.argv[i])
 print("Folder prefixes: " + str(folder_prefixes))
@@ -119,7 +147,10 @@ for i in range(0, int(sys.argv[1])):
 print("Folder suffixes: " + str(folder_suffixes))
 
 all_times = set([])
-times, counts = populate_counts(folder_prefixes, folder_suffixes, all_times)
+times, counts = populate_counts(folder_prefixes, folder_suffixes)
+for prefix in folder_prefixes:
+	for suffix in range(len(folder_suffixes)):
+		all_times = all_times.union(times[prefix][suffix])
 
 # for folder_prefix in folder_prefixes:
 # 	times[folder_prefix] = []
@@ -154,9 +185,9 @@ for j in range(len(folder_prefixes)):
 	i += 1
 
 plt.legend(loc='best')
-plt.ylabel("Max Branches Exercised", fontdict={'size':'12'}, labelpad = 0.05)
-plt.xlabel("Time (hrs)")
-plt.subplots_adjust(left=0.18, bottom=0.17, right=0.98, top=0.99)
+#plt.ylabel("Max Branches Exercised", fontdict={'size':'13'}, labelpad = 0.05)
+#plt.xlabel("Time (hrs)")
+#plt.subplots_adjust(left=0.18, bottom=0.17, right=0.98, top=0.99)
 #bench_name = raw_input("benchmark name: ")
 #print 'saving at: '+ '/home/eecs/clemieux/lfb-figs/24hr-runs/'+ bench_name+ '.png'
 #plt.savefig('/home/eecs/clemieux/lfb-figs/24hr-runs/'+ bench_name+ '.png', format='png', dpi=1000)
