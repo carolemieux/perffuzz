889da6194099fe08781a08e4d10ba5b0fefd57b1
Rohan Padhye <rohanpadhye@cs.berkeley.edu>
2019-03-06 17:51:52 -0800

    Make PERF_SIZE 2^16

--- config.h
+++ config.h
@@ -320,7 +320,7 @@
 /* Map size of the performance bits. These are fine-grained performance
  * profiles (e.g. branch counts or malloc counts).
  */
-#define PERF_SIZE_POW2      14
+#define PERF_SIZE_POW2      16
 #define PERF_SIZE           (1 << PERF_SIZE_POW2)
 
 /* Maximum allocator request size (keep well under INT_MAX): */
